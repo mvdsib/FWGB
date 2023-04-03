59953b073a3fefae8a1ec7958d811415c6359eff
Махиборода Виталий Дмитриевич <mahiboroda_vd@spbstu.ru>
2023-04-03 15:19:15 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -9,3 +9,23 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+Commands();
+string[] array = new string[] {};
+
+string fromUser = ReadInput("Введите команду: ");
+switch (fromUser)
+{
+    case "1":
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case "2":
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{fromUser} - Такой команды нет");
+        break;
+}
