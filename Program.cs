121f01fde8514d9c0e3b3badf2743d8fa1877c8b
Махиборода Виталий Дмитриевич <mahiboroda_vd@spbstu.ru>
2023-04-03 15:19:58 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -29,3 +29,25 @@ switch (fromUser)
         Console.WriteLine($"{fromUser} - Такой команды нет");
         break;
 }
+
+int lenNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) lenNewArray++;
+}
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
