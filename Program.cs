b6287a2cd424d83d16e5ced8a451dad5208e62b8
Махиборода Виталий Дмитриевич <mahiboroda_vd@spbstu.ru>
2023-04-03 14:57:48 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿string[] inputArray = { "hello", "2", "world", ":-)", "1234", "1567" };
+
+// Method for Printing an Array
+string PrintArray(string[] inputArray)
+{
+    return string.Join(", ", inputArray);
+}
+
